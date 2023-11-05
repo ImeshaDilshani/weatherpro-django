6c85ca0433ea674cb28a908ecd176760e689a7dc
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-11-05 15:07:40 +0530

    Initial commit

---
+++ weatherpro/weatherapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import requests
+import datetime
+
+# Create your views here.
+def index(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Amsterdam'
+
+    appid = '19a8a274feb9e734e9d7bc76594478b6'
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q': 'city','appid': appid, 'units': 'metric'}
+    r = requests.get(url=URL, params=PARAMS)
+    res = r.json()
+
+    description = res['weather'][0]['description']
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+    day = datetime.date.today()
+
+    return render(request,
+                  'weatherapp/index.html', {
+                      'description': description,
+                      'icon': icon,
+                      'temp': temp,
+                      'day': day,
+                      'city': city,
+                      }
+                )
